eeee6ad941d8e601e282dde01d991a1c069189fe
fitrisiahaan <fitrisiahaan0612@gmail.com>
2023-12-15 12:24:48 +0700

    first commit

--- src/screens/Add/index.js
+++ src/screens/Add/index.js
@@ -22,34 +22,84 @@ import {
   Center,
   ScrollView,
 } from "@gluestack-ui/themed";
-import Button from "../../components/kecil/button";
-import Input from "../../components/kecil/input";
-import Pilihan from "../../components/kecil/pilihan";
-// import { Button, Input, Pilihan } from "../../components/kecil";
-
+import { Button, Input, Pilihan } from "../../components";
+import { addNote, getNote } from "../../actions/AuthAction";
 const Add = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [status, setStatus] = useState("");
+  const [category, setCategory] = useState("");
+  const [categoryUser, setCategoryUser] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [newCategory, setNewCategory] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
   };
 
+  const toggleAlert = (message) => {
+    setShowAlert(!showAlert);
+    setAlertMessage(message);
+  };
+
+  const ubahStatus = (status) => {
+    setStatus(status);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const notes = await getNote();
+      const categories = notes.map((note) => note.category);
+      const uniqueCategories = categories.filter((value, index, self) => {
+        return self.indexOf(value) === index;
+      });
+      setCategoryUser(uniqueCategories);
+    };
+
+    const unsubscribe = navigation.addListener("focus", fetchData);
+
+    return () => {
+      unsubscribe();
+    };
+  }, [navigation]);
+
+  const onAddNote = async () => {
+    if (title && content && status && category) {
+      const data = {
+        title: title,
+        content: content,
+        status: status,
+        category: category,
+      };
+
+      console.log(data);
+      try {
+        const user = await addNote(data);
+        navigation.replace("MainApp");
+      } catch (error) {
+        console.log("Error", error.message);
+        toggleAlert(error.message);
+      }
+    } else {
+      console.log("Error", "Data tidak lengkap");
+      toggleAlert("Data tidak lengkap");
+    }
+  };
+
+  const handleAddCategory = () => {
+    if (newCategory.trim() !== "") {
+      setCategoryUser((prevCategories) => [...prevCategories, newCategory]);
+      setNewCategory("");
+      setIsModalVisible(false);
+    }
+  };
+
   return (
     <ScrollView>
       <Box flex={1} backgroundColor="$white">
-        <Box
-          shadowColor="$black"
-          shadowOffset={{ width: 0, height: 2 }}
-          shadowOpacity={"$25"}
-          shadowRadius={"$3.5"}
-          elevation={"$5"}
-          backgroundColor="$white"
-          borderRadius={"$md"}
-          mt={"$8"}
-          mx={"$3"}
-          px={"$3"}
-          pt={"$2"}
-        >
+        <Box shadowColor="$black" shadowOffset={{ width: 0, height: 2 }} shadowOpacity={"$25"} shadowRadius={"$3.5"} elevation={"$5"} backgroundColor="$white" borderRadius={"$md"} mt={"$8"} mx={"$3"} px={"$3"} pt={"$2"}>
           <Heading size="2xl" color="$black">
             Add New Task!
           </Heading>
@@ -58,61 +108,48 @@ const Add = ({ navigation }) => {
           </Text>
           <FormControl>
             <Input label={"Title"} width={"$full"} height={"$10"} onChangeText={(title) => setTitle(title)} />
-            <Input textarea={true} label="Content" width={"$full"} height={"$32"} onChangeText={() => {}} />
-            <Pilihan label="Status" />
-            <Pilihan label="Category" />
+            <Input textarea={true} label="Content" width={"$full"} height={"$32"} onChangeText={(content) => setContent(content)} />
+            <Pilihan label="Status" selectedValue={status} onValueChange={(status) => ubahStatus(status)} />
+            <Pilihan label="Category" selectedValue={category} datas={categoryUser} onValueChange={(selectedCategory) => setCategory(selectedCategory)} />
             <Button type="text" title="Add New Category" onPress={toggleModal} padding={10} />
-            <Button type="text" title="Save" padding={10} onPress={() => {}} />
+            <Button
+              type="text"
+              title="Save"
+              padding={10}
+              onPress={() => {
+                onAddNote();
+              }}
+            />
           </FormControl>
         </Box>
 
-        <Modal
-          isOpen={isModalVisible}
-          onClose={toggleModal}
-          finalFocusRef={this.btnRef}
-        >
+        <Modal isOpen={isModalVisible} onClose={toggleModal} finalFocusRef={this.btnRef}>
           <ModalBackdrop />
-          <ModalContent
-            backgroundColor="$white"
-            padding={"$2"}
-            borderRadius={"$lg"}
-          >
+          <ModalContent backgroundColor="$white" padding={"$2"} borderRadius={"$lg"}>
             <ModalHeader>
               <VStack space="sm">
                 <Heading size="lg">Add New Category</Heading>
-                <Text size="sm">
-                  Having a lot of task must be needing categories too!
-                </Text>
+                <Text size="sm">Having a lot of task must be needing categories too!</Text>
               </VStack>
             </ModalHeader>
             <ModalBody>
               <GlueInput>
-                <InputField
-                  role="form"
-                  placeholder="Category Name"
-                  value={null}
-                  onChangeText={() => {}}
-                />
+                <InputField role="form" placeholder="Category Name" value={newCategory} onChangeText={(text) => setNewCategory(text)} />
               </GlueInput>
             </ModalBody>
             <ModalFooter>
-              <Box
-                flex={1}
-                flexDirection="column"
-                justifyContent="space-evenly"
-              >
+              <Box flex={1} flexDirection="column" justifyContent="space-evenly">
                 <Pressable
                   backgroundColor="$blue500"
                   p={"$2"}
                   borderRadius={"$sm"}
                   alignItems="center"
-                  onPress={() => {}} // Trigger category addition
+                  onPress={handleAddCategory} // Trigger category addition
                 >
                   <Text color="$white" fontWeight="$bold">
                     Add
                   </Text>
                 </Pressable>
-
                 <Pressable
                   backgroundColor="$red700"
                   p={"$2"}
@@ -129,6 +166,16 @@ const Add = ({ navigation }) => {
             </ModalFooter>
           </ModalContent>
         </Modal>
+        {/* show Alert */}
+        {showAlert && (
+          <Modal isOpen={showAlert} onClose={toggleAlert}>
+            <ModalBackdrop />
+            <Alert mx="$4" action="error" variant="solid">
+              <AlertText fontWeight="$bold">Error!</AlertText>
+              <AlertText>{alertMessage}</AlertText>
+            </Alert>
+          </Modal>
+        )}
       </Box>
     </ScrollView>
   );
